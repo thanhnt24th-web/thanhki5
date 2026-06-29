35ab4024d8253b3437082106680ac76fe8531011
PMT201 <PMT201@PMT3016>
2026-06-29 15:54:14 +0700

    hoàn thành lab 1

---
+++ WebApplication1/ClassLibrary1/Interfaces/IAuditable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Database.Interfaces
+{
+    public interface IAuditable
+    {
+        public Guid ? CreatedBy { get; set;}
+        public DateTime? Createdon { get; set; }
+        public Guid? ModifiedBy { get; set; }
+        public DateTime? Modifieddon { get; set; }
+        
+    }
+}
---
+++ WebApplication1/ClassLibrary1/Models/Article.cs
@@ -0,0 +1,35 @@
+﻿using Core.Database.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Database.Models
+{
+    [Table("Articles")]
+    public class Articles : IAuditable, IMeta
+    {
+        private string? description;
+
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string Title { get; set; } = " ";
+        [MaxLength(50)]
+        public string? Picture { get; set; }
+        [MaxLength(500)]
+        public string? Intro { get; set; }
+        public string? Content { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public DateTime? Createdon { get; set; }
+        public Guid? ModifiedBy { get; set; }
+        public DateTime? Modifieddon { get; set; }
+        public string? Keyword { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
